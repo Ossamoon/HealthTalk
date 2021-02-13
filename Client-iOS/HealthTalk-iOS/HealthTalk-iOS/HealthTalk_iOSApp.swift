428b75ff9e2b3867b270d4e20b2da9374bd2c8cc
Ossamoon <osamu950623@gmail.com>
2021-02-14 08:22:47 +0900

    Create Xcode project

---
+++ Client-iOS/HealthTalk-iOS/HealthTalk-iOS/HealthTalk_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthTalk_iOSApp.swift
+//  HealthTalk-iOS
+//
+//  Created by 齋藤修 on 2021/02/14.
+//
+
+import SwiftUI
+
+@main
+struct HealthTalk_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
