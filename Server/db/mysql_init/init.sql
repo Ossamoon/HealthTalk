90f5f367c8af00662970cc2bfde4e19b31c96840
Ossamoon <osamu950623@gmail.com>
2021-02-21 01:36:10 +0900

    fix bugs

--- Server/db/mysql_init/init.sql
+++ Server/db/mysql_init/init.sql
@@ -1,9 +1,11 @@
-CREATE DATABASE sample;
-use sample;
+USE go_api_mysql;
 
-CREATE TABLE user (
-    id INT(11) AUTO_INCREMENT NOT NULL,
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(64) NOT NULL,
     password VARCHAR(64) NOT NULL,
-    PRIMARY KEY (id);
+    PRIMARY KEY (id)
 );
+
+INSERT INTO users VALUES (1, 'sampleman', 'samplepass');
+INSERT INTO users VALUES (2, 'testman', 'testpass');
--- Server/model/db.go
+++ Server/model/db.go
@@ -12,13 +12,13 @@ var db *gorm.DB
 func init() {
     user := os.Getenv("MYSQL_USER")
     pass := os.Getenv("MYSQL_PASSWORD")
-    // host := "tcp(127.0.0.1:3306)"
     host := os.Getenv("MYSQL_HOST")
     dbname := os.Getenv("MYSQL_DATABASE")
     connection := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local", user, pass, host, dbname)
 
-    db, err := gorm.Open("mysql", connection)
-    defer db.Close()
+    var err error
+    db, err = gorm.Open("mysql", connection)
+    fmt.Println("Set db:", db)
 
     if err != nil {
         panic("failed to connect database")
