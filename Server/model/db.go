1df4b6bfe361dcecd874eb889136e8e6af22b029
Ossamoon <osamu950623@gmail.com>
2021-02-22 15:12:39 +0900

    create new data-table

--- Server/model/db.go
+++ Server/model/db.go
@@ -3,6 +3,7 @@ package model
 import (
     "fmt"
     "os"
+    
     "github.com/jinzhu/gorm"
     _ "github.com/jinzhu/gorm/dialects/mysql"
 )
--- Server/model/user.go
+++ Server/model/user.go
@@ -1,9 +1,12 @@
 package model
 
+import "time"
+
 type User struct {
-    ID       int   `json:"id" gorm:"primaly_key"`
-    Name     string `json:"name"`
-    Password string `json:"password"`
+    ID        int    `json:"id" gorm:"primaly_key"`
+    Name      string `json:"name"`
+    Password  string `json:"password"`
+    CreatedAt time.Time
 }
 
 func CreateUser(user *User) {
